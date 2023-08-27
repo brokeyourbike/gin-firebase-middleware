0efaff8a6d49cd3522f03a405075ee71a37a3809
Ivan Stasiuk <brokeyourbike@gmail.com>
2023-08-27 18:31:34 +0100

    feat: middleware

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # gin-firebase-middleware
+
+[![Go Reference](https://pkg.go.dev/badge/github.com/brokeyourbike/gin-firebase-middleware.svg)](https://pkg.go.dev/github.com/brokeyourbike/gin-firebase-middleware)
+[![Go Report Card](https://goreportcard.com/badge/github.com/brokeyourbike/gin-firebase-middleware)](https://goreportcard.com/report/github.com/brokeyourbike/gin-firebase-middleware)
+[![Maintainability](https://api.codeclimate.com/v1/badges/a83ce526eca9ba911697/maintainability)](https://codeclimate.com/github/brokeyourbike/gin-firebase-middleware/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/a83ce526eca9ba911697/test_coverage)](https://codeclimate.com/github/brokeyourbike/gin-firebase-middleware/test_coverage)
+
 Assess firebase user info in context
+
+## Installation
+
+```
+go get github.com/brokeyourbike/gin-firebase-middleware
+```
+
+## Docs
+
+- https://cloud.google.com/api-gateway/docs/authenticating-users-firebase
+
+## Authors
+- [Ivan Stasiuk](https://github.com/brokeyourbike) | [Twitter](https://twitter.com/brokeyourbike) | [LinkedIn](https://www.linkedin.com/in/brokeyourbike) | [stasi.uk](https://stasi.uk)
+
+## License
+[BSD-3-Clause License](https://github.com/glocurrency/clearfx-api/blob/main/LICENSE)
---
+++ middleware.go
@@ -0,0 +1,65 @@
+package ginfirebasemiddleware
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+)
+
+// gatewayUserInfoHeader is the header that contains the user info.
+const gatewayUserInfoHeader = "X-Apigateway-Api-Userinfo"
+
+// userInfoCtx is a context key for the UserInfo.
+const userInfoCtx = "FirebaseApiGatewayUserInfo"
+
+type UserInfo struct {
+	Sub   string `json:"sub" binding:"required"`
+	Email string `json:"email" binding:"required,email"`
+}
+
+func Middleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		encodedUser := ctx.GetHeader(gatewayUserInfoHeader)
+		if encodedUser == "" {
+			ctx.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		decodedBytes, err := base64.RawURLEncoding.DecodeString(encodedUser)
+		if err != nil {
+			ctx.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		var userInfo UserInfo
+
+		if err := json.NewDecoder(bytes.NewReader(decodedBytes)).Decode(&userInfo); err != nil {
+			ctx.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		if err := binding.Validator.ValidateStruct(&userInfo); err != nil {
+			ctx.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		ctx.Set(userInfoCtx, userInfo)
+		ctx.Next()
+	}
+}
+
+// GetUserInfo returns the firebase user info from the context.
+func GetUserInfo(ctx *gin.Context) UserInfo {
+	userInfo := ctx.MustGet(userInfoCtx).(UserInfo)
+	return userInfo
+}
+
+// GetUserID returns the firebase user ID from the context.
+func GetUserID(ctx *gin.Context) string {
+	userInfo := GetUserInfo(ctx)
+	return userInfo.Sub
+}
